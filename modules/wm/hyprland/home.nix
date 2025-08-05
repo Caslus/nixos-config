5c0d7d7d511914cac330288e5bb498209859d649
Lucas Philippe <git@lucasphilippe.com>
2025-08-05 18:26:14 -0300

    feat: add multi-gpu

--- modules/wm/hyprland/home.nix
+++ modules/wm/hyprland/home.nix
@@ -21,6 +21,10 @@
         "eDP-1, 2560x1600@240, 0x0, 1"
         "HDMI-A-1, 2560x1440@144, 2560x0, 1"
       ];
+
+      "env" = [
+        "AQ_DRM_DEVICES, /dev/dri/card0:/dev/dri/card1"
+      ];
     };
   };
 }
