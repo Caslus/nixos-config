6e92c04dc540c750582fe5c28d4cf61599096077
Lucas Philippe <git@lucasphilippe.com>
2025-08-05 11:29:23 -0300

    feat: add greeter

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -58,6 +58,15 @@
 
   programs.hyprland.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd hyprland";
+      };
+    };
+  };
+
   programs.firefox.enable = true;
 
   programs.nix-ld.enable = true; # so vscode remote ssh works
