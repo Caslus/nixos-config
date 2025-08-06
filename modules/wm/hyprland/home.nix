99fd900e00f5c516ae275dd13e8376e72cdc2445
Lucas Philippe <git@lucasphilippe.com>
2025-08-06 19:23:03 -0300

    feat: update monitor settings

--- modules/wm/hyprland/home.nix
+++ modules/wm/hyprland/home.nix
@@ -17,8 +17,16 @@
       ];
 
       "monitor" = [
-        "eDP-1, 2560x1600@240, 0x0, 1"
+        "eDP-1, 2560x1600@240, 0x0, 1.5"
         "HDMI-A-1, 2560x1440@144, 2560x0, 1"
+      ];
+
+      "xwayland" = {
+        "force_zero_scaling" = true;
+      };
+
+      "animation" = [
+        "windows, 1, 10, default, gnomed"
       ];
 
       "env" = [
